ceed61a810a3390f1f09c3d7a12fab3fcfaff5ac
Garry <tahinjanaharygarry@gmail.com>
2024-02-29 18:37:05 +0300

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Attendance App')
+    .setDescription('The Attendance API')
+    .setVersion('1.0')
+    .addTag('attendance')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3500);
 }
 bootstrap();
