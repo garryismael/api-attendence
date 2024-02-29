b483003eadcdc6a5918124b5c66cfaac218e1437
Garry <tahinjanaharygarry@gmail.com>
2024-02-29 15:14:50 +0300

    setup employee package

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -3,10 +3,11 @@ import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from './employee.entity';
+import { CreateEmployeeUseCase } from './use-cases/create';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Employee])],
-  providers: [EmployeeService],
+  providers: [EmployeeService, CreateEmployeeUseCase],
   controllers: [EmployeeController],
 })
 export class EmployeeModule {}
--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Employee } from './employee.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+  constructor(
+    @InjectRepository(Employee)
+    private repository: Repository<Employee>,
+  ) {}
+
+  async save(employee: Employee) {
+    return await this.repository.save(employee);
+  }
+}
