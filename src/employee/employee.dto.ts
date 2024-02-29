d004fe790ad31024c71b9dbf2901904e8f965cf9
Garry <tahinjanaharygarry@gmail.com>
2024-02-29 15:14:35 +0300

    add constructor for employee

--- src/employee/employee.dto.ts
+++ src/employee/employee.dto.ts
@@ -2,6 +2,12 @@ export class EmployeeResponseDTO {
   id: number;
   firstName: string;
   lastName: string;
+
+  constructor(id: number, firstName: string, lastName: string) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 }
 
 export class EmployeeRequestDTO {
