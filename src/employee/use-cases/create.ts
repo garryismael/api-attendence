40cf619dc4af670624746768394bda31f3cf2adc
Garry <tahinjanaharygarry@gmail.com>
2024-02-29 15:15:09 +0300

    add feature when creating employee

--- src/employee/use-cases/create.ts
+++ src/employee/use-cases/create.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { EmployeeRequestDTO, EmployeeResponseDTO } from '../employee.dto';
+import { Employee } from '../employee.entity';
+import { EmployeeService } from '../employee.service';
 
 @Injectable()
-export class CreateEmployeeUseCase {}
+export class CreateEmployeeUseCase {
+  constructor(private readonly employeeService: EmployeeService) {}
+
+  async execute(request: EmployeeRequestDTO): Promise<EmployeeResponseDTO> {
+    let employee = new Employee();
+    employee.firstName = request.firstName;
+    employee.lastName = request.lastName;
+    employee = await this.employeeService.save(employee);
+
+    return new EmployeeResponseDTO(
+      employee.id,
+      employee.firstName,
+      employee.lastName,
+    );
+  }
+}
