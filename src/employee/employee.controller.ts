521d0664aba79ec65ec37361f3211a3ac6ae13d5
Garry <tahinjanaharygarry@gmail.com>
2024-02-29 15:14:14 +0300

    create employee controller

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { EmployeeRequestDTO } from './employee.dto';
+import { CreateEmployeeUseCase } from './use-cases/create';
 
-@Controller('employee')
-export class EmployeeController {}
+@Controller('employees')
+export class EmployeeController {
+  constructor(private createUseCase: CreateEmployeeUseCase) {}
+  @Post()
+  async create(@Body() request: EmployeeRequestDTO) {
+    return await this.createUseCase.execute(request);
+  }
+}
