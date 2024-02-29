96d577f36b48edc2409921aad01bf51723d8e7dd
Garry <tahinjanaharygarry@gmail.com>
2024-02-29 18:20:52 +0300

    create methods

--- src/attendance/attendance.service.ts
+++ src/attendance/attendance.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Attendance } from './attendance.entity';
-import { DepartmentService } from '@/departments/departments.service';
-import { EmployeeService } from '@/employee/employee.service';
-import { AttendanceDateService } from '@/attendance-date/attendance-date.service';
 
 @Injectable()
 export class AttendanceService {
   constructor(
     @InjectRepository(Attendance)
     private repository: Repository<Attendance>,
-    private departmentService: DepartmentService,
-    private employeeService: EmployeeService,
-    private attendanceDateService: AttendanceDateService,
   ) {}
+
+  async save(attendance: Attendance) {
+    return this.repository.save(attendance);
+  }
+
+  async bulkSave(attendances: Attendance[]) {
+    return this.repository.save(attendances);
+  }
 }
