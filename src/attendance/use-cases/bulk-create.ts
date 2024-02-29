819f05ab7ce6c2f8cfd9eadd568b4c83f66865f2
Garry <tahinjanaharygarry@gmail.com>
2024-02-29 18:43:08 +0300

    split codes

--- src/attendance/use-cases/bulk-create.ts
+++ src/attendance/use-cases/bulk-create.ts
@@ -1,11 +1,16 @@
 import { AttendanceDate } from '@/attendance-date/attendance-date.entity';
 import { AttendanceDateService } from '@/attendance-date/attendance-date.service';
+import { Department } from '@/departments/department.entity';
 import { DepartmentService } from '@/departments/departments.service';
 import { Employee } from '@/employee/employee.entity';
 import { EmployeeService } from '@/employee/employee.service';
 import { parseToDate } from '@/utils';
 import { Injectable } from '@nestjs/common';
-import { BulkAttendanceRequestDTO } from '../attendance.dto';
+import {
+  AttendanceDateEmployeeRequestDTO,
+  AttendanceEmployeeRequestDTO,
+  BulkAttendanceRequestDTO,
+} from '../attendance.dto';
 import { Attendance } from '../attendance.entity';
 import { AttendanceService } from '../attendance.service';
 
@@ -24,41 +29,53 @@ export class BulkCreateAttendanceUseCase {
         return await this.departmentService.findOrCreateByName(item.department);
       }),
     );
-
     await Promise.all(
       request.employees.map(async (item, index) => {
-        let employee = new Employee();
-        employee.id = item.id;
-        employee.firstName = item.firstName;
-        employee.lastName = item.lastName;
-        employee.department = departments[index];
-        employee = await this.employeeService.save(employee);
-        return employee;
+        return await this.createEmployee(item, departments, index);
       }),
     );
-
     const attendancesDates = await Promise.all(
       request.attendances.map(async (item) => {
-        let attendanceDate = new AttendanceDate();
-        attendanceDate.date = parseToDate(item.date);
-        attendanceDate = await this.attendanceDateService.save(attendanceDate);
-        return attendanceDate;
+        return await this.createAttendanceDate(item);
       }),
     );
-
-    const attendances = await Promise.all(
+    return await Promise.all(
       request.attendances.map(async (item, index) => {
-        let attendance = new Attendance();
-        attendance.date = attendancesDates[index];
-        attendance.present = item.present;
-        attendance.employee = await this.employeeService.findOneById(
-          item.employeeId,
-        );
-        attendance = await this.attendanceService.save(attendance);
-        return attendance;
+        return await this.createAttendance(attendancesDates, index, item);
       }),
     );
+  }
 
-    return attendances;
+  private async createAttendance(
+    attendancesDates: AttendanceDate[],
+    index: number,
+    item: AttendanceDateEmployeeRequestDTO,
+  ) {
+    const attendance = new Attendance();
+    attendance.date = attendancesDates[index];
+    attendance.present = item.present;
+    attendance.employee = await this.employeeService.findOneById(
+      item.employeeId,
+    );
+    return await this.attendanceService.save(attendance);
+  }
+
+  private async createAttendanceDate(item: AttendanceDateEmployeeRequestDTO) {
+    const attendanceDate = new AttendanceDate();
+    attendanceDate.date = parseToDate(item.date);
+    return await this.attendanceDateService.save(attendanceDate);
+  }
+
+  private async createEmployee(
+    item: AttendanceEmployeeRequestDTO,
+    departments: Department[],
+    index: number,
+  ) {
+    const employee = new Employee();
+    employee.id = item.id;
+    employee.firstName = item.firstName;
+    employee.lastName = item.lastName;
+    employee.department = departments[index];
+    return await this.employeeService.save(employee);
   }
 }
