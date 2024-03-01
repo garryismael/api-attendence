508b9af2f169ab16794fc55db5dfb5f509989fb7
Garry <tahinjanaharygarry@gmail.com>
2024-03-01 07:39:01 +0300

    setup find one controller

--- src/departments/departments.controller.ts
+++ src/departments/departments.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { FindDepartmentsUseCase } from './use-cases/find-all';
+import { FindOneDepartmentUseCase } from './use-cases/find-one';
 
 @Controller('departments')
 export class DepartmentsController {
-  constructor(private findUseCase: FindDepartmentsUseCase) {}
+  constructor(
+    private findUseCase: FindDepartmentsUseCase,
+    private readonly findOneUseCase: FindOneDepartmentUseCase,
+  ) {}
 
   @Get()
   findAll() {
     return this.findUseCase.execute();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.findOneUseCase.execute(id);
   }
 }
--- src/departments/departments.module.ts
+++ src/departments/departments.module.ts
@@ -4,11 +4,16 @@ import { DepartmentsController } from './departments.controller';
 import { Department } from './department.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FindDepartmentsUseCase } from './use-cases/find-all';
+import { FindOneDepartmentUseCase } from './use-cases/find-one';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Department])],
   exports: [DepartmentService],
-  providers: [DepartmentService, FindDepartmentsUseCase],
+  providers: [
+    DepartmentService,
+    FindDepartmentsUseCase,
+    FindOneDepartmentUseCase,
+  ],
   controllers: [DepartmentsController],
 })
 export class DepartmentsModule {}
