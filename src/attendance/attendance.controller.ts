da8b97b96212cb2765c7b5ec9d476b5e2aa941cc
Garry <tahinjanaharygarry@gmail.com>
2024-03-01 07:47:20 +0300

    config swagger api

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { BulkCreateAttendanceUseCase } from './use-cases/bulk-create';
 import {
   AttendanceRequestDTO,
@@ -8,7 +17,8 @@ import { FindAllAttendanceUseCase } from './use-cases/find-all';
 import { DeleteAttendanceUseCase } from './use-cases/delete';
 import { FindOneAttendanceUseCase } from './use-cases/find-one';
 import { CreateAttendanceUseCase } from './use-cases/create';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Attendance } from './attendance.entity';
 
 @Controller('attendances')
 @ApiTags('Attendances')
@@ -22,21 +32,37 @@ export class AttendanceController {
   ) {}
 
   @Post()
+  @HttpCode(201)
+  @ApiOkResponse({
+    type: Attendance,
+    status: HttpStatus.CREATED,
+  })
   create(@Body() request: AttendanceRequestDTO) {
     return this.createUseCase.execute(request);
   }
 
   @Get()
+  @ApiOkResponse({
+    type: Array<Attendance>,
+  })
   findAll() {
     return this.findAllUseCase.execute();
   }
 
   @Get(':id')
+  @ApiOkResponse({
+    type: Attendance,
+  })
   findOne(@Param('id') id: number) {
     return this.findOneUseCase.execute(id);
   }
 
   @Post('bulk-create')
+  @HttpCode(201)
+  @ApiOkResponse({
+    type: Attendance,
+    status: HttpStatus.CREATED,
+  })
   bulkSave(@Body() request: BulkAttendanceRequestDTO) {
     return this.bulkCreateUseCase.execute(request);
   }
--- src/attendance/attendance.dto.ts
+++ src/attendance/attendance.dto.ts
@@ -1,24 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class AttendanceEmployeeRequestDTO {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   firstName: string;
+  @ApiProperty()
   lastName: string;
+  @ApiProperty()
   department: string;
 }
 
 export class AttendanceDateEmployeeRequestDTO {
+  @ApiProperty()
   employeeId: number;
+  @ApiProperty()
   date: string;
+  @ApiProperty()
   present: boolean;
 }
 
 export class BulkAttendanceRequestDTO {
+  @ApiProperty()
   employees: Array<AttendanceEmployeeRequestDTO>;
+  @ApiProperty()
   attendances: Array<AttendanceDateEmployeeRequestDTO>;
 }
 
 export class AttendanceRequestDTO {
+  @ApiProperty()
   employeeId: number;
+  @ApiProperty()
   date: string;
+  @ApiProperty()
   present: boolean;
 }
 
