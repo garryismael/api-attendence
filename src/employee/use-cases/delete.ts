f141ac6994abcc70d10b86e257e06e7cade86358
Garry <tahinjanaharygarry@gmail.com>
2024-03-01 07:25:33 +0300

    add delete feature

---
+++ src/employee/use-cases/delete.ts
@@ -0,0 +1,14 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { EmployeeService } from '../employee.service';
+
+@Injectable()
+export class DeleteEmployeeUseCase {
+  constructor(private readonly employeeService: EmployeeService) {}
+  async execute(id: number) {
+    const employee = await this.employeeService.findOneById(id);
+    if (!employee) {
+      throw new NotFoundException('Employee Not Found');
+    }
+    return this.employeeService.deleteById(id);
+  }
+}
